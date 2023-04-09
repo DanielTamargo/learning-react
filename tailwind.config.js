0bf35be94c9dc3d58b89080cc2f583e91a554535
DanielTamargo <danieltamargosaiz@gmail.com>
2023-04-09 19:21:21 +0200

    Setting up my very first react project

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+
+      colors: {
+        'main-dark': '#1C0B19',
+        'main-blue': '#140D4F',
+        'main-light': '#4EA699',
+        'main-success': '#2DD881',
+      }
     },
   },
   plugins: [],
